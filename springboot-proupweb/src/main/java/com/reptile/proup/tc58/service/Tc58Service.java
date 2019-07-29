a1a536315fd11a25819977a71d38cba0addbf95d
shiwei <yangshiwei@sinosoft.com.cn>
2019-07-29 17:52:12 +0800

    拆分分支

--- springboot-proupweb/src/main/java/com/reptile/proup/tc58/service/Tc58Service.java
+++
@@ -1,9 +0,0 @@
-package com.reptile.proup.tc58.service;
-
-public interface Tc58Service {
-	//向数据库中更新58同城的城市信息
-	public boolean cityInfoUpdate(String page);
-
-	//更新58同城在DB中参数的映射
-	public boolean updateQueryParams(String page);
-}
