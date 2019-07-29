d762bd7ae6ae6cd807cff19ff956c4c18850ee62
shiwei <yangshiwei@sinosoft.com.cn>
2019-07-29 18:04:53 +0800

    拆分分支

--- springboot-reptile-boss/src/main/java/com/reptile/boss/service/Boss2DBService.java
+++
@@ -1,6 +0,0 @@
-package com.reptile.boss.service;
-
-
-public interface Boss2DBService {
-	public String getBrowserUserAgent(String agentid);
-}
--- springboot-reptile-lagou/src/main/java/com/reptile/lagou/service/LagouPageService.java
+++
@@ -1,9 +0,0 @@
-package com.reptile.lagou.service;
-
-import java.util.List;
-
-public interface LagouPageService {
-
-	//获取页面职位信息
-	public <T> List<T> getJobList(String pageDoc);
-}
